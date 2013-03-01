695239da52d03ae8568f4c511bcecde73b1f5499
root <penguin6160@sbcglobal.net>
2013-03-01 13:31:02 +0000

    Empty home page

--- home/views.py
+++ home/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+def Home(request):
+  return render_to_response('home.html')
+
--- ridemix/urls.py
+++ ridemix/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ridemix.views.home', name='home'),
+    url(r'^$', 'home.views.Home'),
     # url(r'^ridemix/', include('ridemix.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:

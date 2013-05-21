7428e25edcc29733bead09eecc7b4acbcd7a3a70
root <penguin6160@sbcglobal.net>
2013-05-21 12:52:15 +0000

    stuff for the mechanical turk instance

--- ridemix/static/js/news.js
+++ ridemix/static/js/news.js
@@ -1,17 +1,34 @@
 $(function() {
-    fetchNewsTopics();    
+    fetchNewsTopics(); 
 });
 
 function addToNewsList(container, index) {
     var topic = window.NEWS[index][0];
     var news_id = index + "_topic";
-    var anchor = $("<a>");
-    anchor.attr('id', news_id + "_div");
-    anchor.attr('data-role', "button");
-    anchor.attr('href', "https://blekko.com/ws/?q=" + encodeURIComponent(topic) + "+%2Fnews-magazine");
-    anchor.attr('target', "_blank");
-    anchor.text(topic);
-    container.append(anchor);
+    var collapsible = $("<div>");
+    collapsible.attr('data-role', "collapsible");
+    collapsible.attr('id', news_id + "_collapsible");
+//    collapsible.attr('data-mini', "true");
+    var title = $("<h3>");
+    title.text(topic);
+    title.attr('class', "ui-collapsible-heading");
+    title.attr('onclick', "expandNews(" + index + ")");
+    var list = $("<ul>");
+    list.attr('id', news_id + "_list");
+    list.attr('data-role', "listview");
+//    list.attr('data-theme', "d");
+//    var anchor = $("<a>");
+//    anchor.attr('id', news_id + "_anchor");
+//    anchor.attr('data-role', "button");
+//    anchor.attr('href', "https://blekko.com/ws/?q=" + encodeURIComponent(topic) + "+%2Fnews-magazine");
+//    anchor.attr('href', "#");
+//    anchor.attr('onclick', "expandNews(" + index + ")");
+//    anchor.attr('target', "_blank");
+//    anchor.text(topic);
+//    div.append(anchor);
+    collapsible.append(title);
+    collapsible.append(list);
+    container.append(collapsible);
 }    
 
 function fetchNewsTopics() {
@@ -35,4 +52,44 @@ function fetchNewsTopics() {
         }
         container.trigger('create');
     });
+}
+
+function expandNews(index) {
+    var container = $('#' + index + "_topic_list");
+    var alreadyThere = container.innerHTML;
+    if (typeof alreadyThere === 'undefined' || alreadyThere == "") {
+        var topic = window.NEWS[index][0];
+        var url = '/get/bingnews?topic=' + encodeURIComponent(topic);
+        $.mobile.loading();
+        window.bing_ajax = $.ajax({
+            'url' : url,
+            'datatype' : 'text/json',
+        }).done(function(data) {
+            for (var i = 0; i < data.d.results[0].News.length; i++) {
+                var url = data.d.results[0].News[i].Url;
+                var title = data.d.results[0].News[i].Title;
+                var source = data.d.results[0].News[i].Source;
+                var description = data.d.results[0].News[i].Description;
+                var list_item = $("<li>");
+                //list_item.attr('data-theme', "d");
+                var anchor = $("<a>");
+                //anchor.attr('data-role', "button");
+                anchor.attr('href', url);
+                //anchor.text(title);
+                anchor.attr('target', "_blank");
+                var title_text = $("<h3>");
+                title_text.text(title + " --- (" + source + ")");
+                title_text.attr('class', "ui-li-heading");
+                //var source_text = $("<p>");
+                //source_text.text(source);
+                //source_text.attr('class', "ui-li-aside"); 
+                anchor.append(title_text);
+                list_item.append(anchor);
+                //list_item.append(source_text);
+                container.append(list_item);
+            }
+            container.trigger('create');
+            $.mobile.loading();
+        });
+    }
 }
